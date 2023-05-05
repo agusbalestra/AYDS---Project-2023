a993c96b13eb32b3e7e1db1be633b515a8111f1e
matybq <matybq20@gmail.com>
2023-05-05 11:15:28 -0300

    app dockerized, html changes

--- cheftravel/server.rb
+++ cheftravel/server.rb
@@ -5,22 +5,29 @@ require 'sinatra/reloader'
  
 set :database, {adapter: "sqlite3", database: "db.sqlite3"}
 
-class User < ActiveRecord::Base
+class App < Sinatra::Application
+  def initialize(app = nil)
+    super()
 end
+
+class User < ActiveRecord::Base
+end 
 
 get '/' do
   erb :index
 end
 
+# Crea o encuentra un usurio en la bdd dependiendo del nombre que inserte
 post '/users' do
   @user = User.find_or_create_by(name: params[:name])
-
+  
   erb :user
 end
 
 # Lista todos los usuarios registrados en la base de datos
 get '/users' do
   @users = User.all
-
+  
   erb :users
+end
 end
