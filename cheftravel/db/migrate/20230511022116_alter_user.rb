0c67d46617d06219d9a04c543a28156b1b4dca4c
matybq <matybq20@gmail.com>
2023-05-11 10:50:47 -0300

    alteruser

---
+++ cheftravel/db/migrate/20230511022116_alter_user.rb
@@ -0,0 +1,8 @@
+class AlterUser < ActiveRecord::Migration[7.0]
+
+    add_column :users, :password, :string
+    add_column :users, :firstname, :string
+    add_column :users, :lastname, :string
+    add_column :users, :email, :string
+
+end
--- cheftravel/db/schema.rb
+++ cheftravel/db/schema.rb
@@ -10,12 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_01_204440) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_11_022116) do
   create_table "users", force: :cascade do |t|
     t.string "name"
-
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
+    t.string "firstname"
+    t.string "lastname"
+    t.string "email"
   end
 
 end
