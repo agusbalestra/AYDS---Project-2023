587a0b036abac93a417dffff42604ba21b7b3880
agusbalestra <agusbalestra0@gmail.com>
2023-05-21 20:56:26 -0300

    fix code

--- cheftravel/server.rb
+++ cheftravel/server.rb
@@ -38,19 +38,26 @@ class App < Sinatra::Application
     user_exists = User.find_by(name: @name)
 
     if @reppw != @password
+
       @msgfail = "Las contraseñas no coinciden"
+      
       erb :fail_register
     elsif @password.length < 6
+
       @msgfail = "Su contraseña es muy corta. El mínimo es de 6 caracteres"
+
       erb :fail_register
     elsif user_exists != nil
+
       @msgfail = "Ya existe un usuario con ese nombre de usuario. Por favor, elige otro"
+
       erb :fail_register
     else
       user = User.create(name: @name, password: @password,
                          firstname: @firstname, lastname: @lastname,
                          email: @email, points: 0)
       session[:user_id] = user.id
+
       erb :argentina
     end
   end
@@ -60,11 +67,9 @@ class App < Sinatra::Application
 
     if @user
       session[:user_id] = @user.id
-
-      erb :argentina
+      redirect '/menu'
     else
       @msg = "Usuario y/o contraseña incorrectos. Inténtalo nuevamente"
-
       erb :fail_login
     end
   end
@@ -72,6 +77,7 @@ class App < Sinatra::Application
   get '/menu' do
     redirect '/' unless current_user
 
+    @user = current_user
     erb :argentina
   end
 
@@ -98,33 +104,31 @@ class App < Sinatra::Application
   post '/question' do
     option_id = params[:option_id].to_i
     option = Answer.find(option_id)
-
+    @question = Question.find(params[:question_id])
+    @user = current_user
+  
     if option.correct
-
       result_message = "¡CORRECTO!"
       if @question.difficulty.to_i == 1
-        current_user.update(points: current_user.points.to_i + 10)
+        @user.update(points: @user.points.to_i + 10)
       elsif @question.difficulty.to_i == 2
-        current_user.update(points: current_user.points.to_i + 20)
+        @user.update(points: @user.points.to_i + 20)
       else
-        current_user.update(points: current_user.points.to_i + 30)
+        @user.update(points: @user.points.to_i + 30)
       end
-
     else
-
       result_message = "INCORRECTO"
       if @question.difficulty.to_i == 1
-        current_user.update(points: current_user.points.to_i - 10)
+        @user.update(points: @user.points.to_i - 10)
       elsif @question.difficulty.to_i == 2
-        current_user.update(points: current_user.points.to_i - 20)
+        @user.update(points: @user.points.to_i - 20)
       else
-        current_user.update(points: current_user.points.to_i - 30)
+        @user.update(points: @user.points.to_i - 30)
       end
-
     end
-
+  
     erb :result, locals: { result_message: result_message, id: @question.id }
-  end
+  end   
 
   def current_user
     User.find_by(id: session[:user_id])
