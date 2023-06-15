a5a37634231c438149a20d0fe6ad1396976a3fbb
agusbalestra <agusbalestra0@gmail.com>
2023-06-15 16:42:28 -0300

    add some tests

--- cheftravel/spec/models/user_spec.rb
+++ cheftravel/spec/models/user_spec.rb
@@ -1,13 +1,37 @@
+# spec/models/user_spec.rb
 require 'sinatra/activerecord'
 require_relative '../../models/init.rb'
 
-describe 'User' do
-  describe 'valid' do
-    describe 'when there is no name' do
-      it 'should be invalid' do
-        u = User.new
-        expect(u.valid?).to eq(false)
-      end
-    end
+describe User do
+  it "is valid with a username and email" do
+    user = User.new(name: "Juan", email: "riquelme10@gmail.com", password: "seremosmenosmalosquelosdemas")
+    expect(user).to be_valid
+  end
+
+  it "is invalid without a username" do
+    user = User.new(email: "romi10@gmail.com", password: "boca123")
+    expect(user).not_to be_valid
+  end
+
+  it "is invalid without an email address" do
+    user = User.new(name: "marten", password: "bocaaa")
+    expect(user).not_to be_valid
+  end
+
+  it "is invalid without an password" do
+    user = User.new(name: "liomessi10", email: "soydeboca@gmail.com")
+    expect(user).not_to be_valid
+  end
+
+  it "is invalid with a duplicate email address" do
+    existing_user = User.create(name: "pichonadvincula", email: "peru@example.com", password: "zambradios")
+    user = User.new(name: "cheftravel", email: "peru@example.com", password: "latercera")
+    expect(user).not_to be_valid
+  end
+
+  it "is invalid with a duplicate username" do
+    existing_user = User.create(name: "MarcosRED", email: "bocaelmasgrande@example.com", password: "matafuegos123")
+    user = User.new(name: "MarcosRED", email: "tefuistealab@mail.com", password: "password")
+    expect(user).not_to be_valid
   end
 end
