b0347927ec93e14e858706232a64bdbb7f4f52ee
agusbalestra <agusbalestra0@gmail.com>
2023-09-05 10:46:23 -0300

    tests for answers. Ref #7

--- cheftravel/models/answer.rb
+++ cheftravel/models/answer.rb
@@ -3,5 +3,5 @@ require 'active_record'
 class Answer < ActiveRecord::Base
   belongs_to :question
   validates :text, presence: true
-  validates :question, presence: true
+  validates :question, presence: true, uniqueness: true
 end
--- cheftravel/spec/models/answer_spec.rb
+++ cheftravel/spec/models/answer_spec.rb
@@ -2,18 +2,16 @@ require 'sinatra/activerecord'
 require_relative '../../models/init.rb'
 
 describe Answer do
-  
-  it "answer ref to one question" do
-    question = Question.new(id: 1, text: "Cual es el manjar tipico de Argentina?", difficulty: 1)
-    question2 = Question.new(id: 2, text: "¿Cual es el punto del asado?",difficulty: 2)
-    answer = Answer.new(text: "Dulce De Leche", correct: true, question_id: question.id)
-    expect(answer.question_id). to eq(question.id)
-    expect(answer.question_id). not_to eq(question2.id)
-  end
 
   it "answer have a question" do
     answer = Answer.new(text: "Dulce De Leche", correct: true)
-    expect(answer). not_to be_valid
+    expect(answer.valid?). to be_falsey
   end
 
+  it "answer valid" do
+    question = Question.new(id: 1, text: "Cual es el manjar tipico de Argentina?", difficulty: 1)
+    answer = Answer.new(text: "Dulce De Leche", correct: true, question: question)
+    expect(answer.valid?).to be_truthy
+  end
+  
 end
