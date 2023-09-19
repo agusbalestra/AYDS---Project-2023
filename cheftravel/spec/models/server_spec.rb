7bff1395c451b131319413c67bf37c58f8c48550
agusbalestra <agusbalestra0@gmail.com>
2023-09-19 11:29:49 -0300

    [#186017647] - some coverage tests.

--- cheftravel/spec/models/server_spec.rb
+++ cheftravel/spec/models/server_spec.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 require 'rack/test'
 require 'sinatra/activerecord'
-require_relative '../../models/init.rb'
-require_relative '../../server.rb'
-
+require_relative '../../models/init'
+require_relative '../../server'
 
 RSpec.describe 'Sinatra App' do
   include Rack::Test::Methods
@@ -20,6 +21,26 @@ RSpec.describe 'Sinatra App' do
     expect(last_response.status).to eq(200)
   end
 
+  context 'POST /registermenu' do
+    it 'successful register' do
+      user_params = { name: 'colo', password: 'aguanteboca123' }
+      user = User.new(user_params)
+      allow(user).to receive(:valid?).and_return(true)
+      allow(user).to receive(:save).and_return(true)
+      expect(User).to receive(:new).with(user_params).and_return(user)
+      expect(user).to receive(:set_default_points)
+      expect(user).to receive(:save)
+      # POST
+      post '/registermenu', user_params
+    end
+
+    it 'failed register' do
+      # incorrect params
+      post '/registermenu', { name: 'a', password: 'a' }
+      expect(last_response.body).to include('user.errors.fullmesages')
+    end
+  end
+
   it 'dont exist endpoint' do
     get '/logmenu'
     expect(last_response.status).to eq(404)
@@ -30,16 +51,16 @@ RSpec.describe 'Sinatra App' do
     expect(last_response.status).to eq(302)
   end
 
-  it 'testing menu' do
-    post '/logmenu', {name: "colo", password: "aguanteboca123"}
-    get '/menu'
-    expect(last_response.status).to eq(200)
+  it 'successful login' do
+    post '/logmenu', { name: 'colo', password: 'aguanteboca123' }
+    expect(last_response).to be_redirect
+    follow_redirect!
+    expect(last_request.path).to eq('/menu')
   end
 
-  it 'testing chile' do
-    post '/logmenu', {name: "colo", password: "aguanteboca123"}
-    get '/chile'
-    expect(last_response.status).to eq(200)
+  it 'failed login' do
+    post '/logmenu', { name: 'a', password: 'a' }
+    expect(last_response).to be_ok
+    expect(last_response.body).to include('Usuario y/o contraseña incorrectos. Inténtalo nuevamente')
   end
-
 end
