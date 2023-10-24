df5fc01efdab5f678c944cf2719a9898cf0471d9
matybq <matybq20@gmail.com>
2023-10-24 12:20:31 -0300

    working

--- cheftravel/models/user.rb
+++ cheftravel/models/user.rb
@@ -1,18 +1,17 @@
 require_relative 'question'
 require_relative 'correct_questions'
+require 'bcrypt'
 
 class User < ActiveRecord::Base
+  include BCrypt
+  
   has_many :correct_questions
   has_many :questions, through: :correct_questions
 
-  has_secure_password
-
   validates :name, presence: true, uniqueness: true, length: { in: 3..20 }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "debe ser una dirección de correo electrónico válida" } # verifica que haya un caracter antes y dsp del @
-  validates :password, presence: true, length: { minimum: 4, too_short: "te queres ir hackeado?" }
-  validates :password, presence: true, length: { minimum: 4, too_short: "te queres ir hackeado?" }, unless: :auth_with_google
   
-  validate :password_match, unless: :auth_with_google
+  validate :has_secure_password, unless: :auth_with_google
   after_initialize :set_default_points
 
   attr_accessor :reppw
@@ -20,6 +19,14 @@ class User < ActiveRecord::Base
 
   def set_default_points
     self.points ||= 0
+  end
+
+  def has_secure_password
+    self.password_digest = Password.create(password_digest)
+  end
+
+  def authenticate(entered_password)
+    Password.new(password_digest) == entered_password
   end
 
   def points_treatment(correct, diff)
